dc20caa7c9f5b0c2d3766fb1f43539524ce688f8
vaishnavi dhage <vaishnavidhage717@gmail.com>
2024-07-27 20:57:07 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+tailwind.config= {
+    theme:{
+        extend:{
+            gridTemplateColumns:{
+                'auto':'repeat(auto-fit, minmax(200px, 1fr))'
+            },
+            animation:{
+                spin_slow:'spin 6s linear infinite'
+            },
+            colors:{
+                lightHover:'#fcf4ff',
+                darkHover:'#2a004a',
+                darkTheme:'#11001F'
+
+            },
+            boxShadow:{
+                'black':'4px 4px 0 #000',
+                'white':'4px 4px 0 #fff',
+            }
+    }
+},
+        darkMode:'selector'
+}
